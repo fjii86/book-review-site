c877d4a914e48a5b514b5e1d536b48214dd85131
fjii86 <fjii_@hotmail.com>
2025-07-03 00:30:58 +0200

    Add separate review url

--- books/forms.py
+++ books/forms.py
@@ -4,19 +4,12 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['rating', 'content']  # Viktigt!
+        fields = ['book', 'rating', 'content']  # Viktigt!
         widgets = {
-            'rating': forms.NumberInput(attrs={
-            'min': 1, 
-            'max': 5, 
-            'class': 'form-control w-auto',
-            'style': 'display: inline-block; width: 80px;',
-        }),
-        'content': forms.Textarea(attrs={
-        'rows': 3, 
-        'class': 'form-control'
-        }),
-        } 
+        'book': forms.Select(attrs={'class': 'form-select'}),
+        'rating': forms.NumberInput(attrs={'min': 1, 'max': 5, 'class': 'form-control w-auto', 'style': 'width: 80px'}),
+        'content': forms.Textarea(attrs={'rows': 3, 'class': 'form-control'}),
+} 
         labels = {
             'rating': 'Rating (1–5)',
             'content': 'Your Review',
--- books/urls.py
+++ books/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('signup/', views.signup, name='signup'),
     path('book/<int:pk>/', views.book_detail, name='book_detail'),
+    path('review/new/', views.add_review, name='add_review'),
 ]
