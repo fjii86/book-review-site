76ab0ee539f2b3522c0bfa090a887e57446179f4
fjii86 <fjii_@hotmail.com>
2025-07-02 16:39:49 +0200

    Add book and review model

--- plotpointreviews/books/models.py
+++ plotpointreviews/books/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.PositiveSmallIntegerField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Review of {self.book.title} by {self.user.username}'
