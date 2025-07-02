f89c1ae5073b7619ec55953e5ad77f92f0c6feb4
fjii86 <fjii_@hotmail.com>
2025-07-02 21:22:17 +0200

    Add register and login views

--- plotpointreviews/books/urls.py
+++ plotpointreviews/books/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.book_list, name='book_list'),
+    path('signup/', views.signup, name='signup'),
 ]
--- plotpointreviews/books/views.py
+++ plotpointreviews/books/views.py
@@ -6,3 +6,13 @@ from .models import Book
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'books/book_list.html', {'books': books})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Send the user to login after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
