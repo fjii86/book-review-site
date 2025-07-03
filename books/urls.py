21202e4fd2b8e7501cac875329804bfec69c55c7
fjii86 <fjii_@hotmail.com>
2025-07-03 03:00:37 +0200

    Add template to edit review

--- books/urls.py
+++ books/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('book/<int:pk>/', views.book_detail, name='book_detail'),
     path('review/new/', views.add_review, name='add_review'),
+    path('review/<int:pk>/edit/', views.edit_review, name='edit_review'),
+    path('review/<int:pk>/delete/', views.delete_review, name='delete_review'),
     path('add/', views.add_book, name='add_book'),
+
 ]
